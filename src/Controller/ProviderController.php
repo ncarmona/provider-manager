f3e174efaa479984deb5b711820be8add285ca3c
noel_ES <ncarm89@gmail.com>
2022-11-08 12:01:47 +0100

    Show providers table in /

--- src/Controller/ProviderController.php
+++ src/Controller/ProviderController.php
@@ -10,9 +10,7 @@ class ProviderController extends AbstractController {
      * @route("/", name="provider_list")
      */
     public function list(): Response {
-        return new Response(
-            '<html><body>List of all providers</body></html>'
-        );
+        return $this->render('providers/list/table.html.twig');
     }
 }
 ?>
