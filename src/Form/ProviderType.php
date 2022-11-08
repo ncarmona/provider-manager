58fbaf26f8a2db14a3cf6b38031b2049ed9a7484
noel_ES <ncarm89@gmail.com>
2022-11-08 18:17:45 +0100

    Assigned providers field types

--- src/Form/ProviderType.php
+++ src/Form/ProviderType.php
@@ -6,18 +6,30 @@ use App\Entity\Provider;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProviderType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('phone')
-            ->add('active')
-            ->add('type')
-        ;
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('phone', TelType::class)
+            ->add('active', CheckboxType::class)
+            ->add('type',ChoiceType::class, [
+                'choices'  => [
+                    'Maybe' => 1,
+                    'Yes' => 2,
+                    'No' => 3,
+                ],
+            ])
+            ->add('send', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
