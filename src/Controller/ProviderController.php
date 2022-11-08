6a2bcf52788eac288b51fccc9372c148e010f610
noel_ES <ncarm89@gmail.com>
2022-11-08 18:18:38 +0100

    persist providers in database

--- src/Controller/ProviderController.php
+++ src/Controller/ProviderController.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Form\ProviderType;
 use App\Entity\Provider;
+// use App\Controller\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProviderController extends AbstractController {
     /**
@@ -17,11 +19,20 @@ class ProviderController extends AbstractController {
     /**
      * @route("/create", name="provider_create")
      */
-    public function create(): Response {
+    public function create(Request $request): Response {
         $provider = new Provider();
-        $form = $this->createForm(ProviderType::class, $provider);
+        $form = $this->createForm(ProviderType::class, $provider); 
         $render_parameters = array('provider_form' => $form->createView());
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()) {
+            $provider = $form->getData();
+            $doctrine_manager = $this->getDoctrine()->getManager();
+            $doctrine_manager->persist($provider);
+            $doctrine_manager->flush();
+
+            return $this->redirectToRoute('provider_list');
+        }
         return $this->render('providers/form/form.html.twig', $render_parameters);
     }
 }
