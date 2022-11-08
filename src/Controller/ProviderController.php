9f874966a38d6e8785d94131375f473ac48e15d0
noel_ES <ncarm89@gmail.com>
2022-11-08 16:48:03 +0100

    Calling provider form in create provider

--- src/Controller/ProviderController.php
+++ src/Controller/ProviderController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ProviderType;
+use App\Entity\Provider;
 
 class ProviderController extends AbstractController {
     /**
@@ -11,6 +13,16 @@ class ProviderController extends AbstractController {
      */
     public function list(): Response {
         return $this->render('providers/list/table.html.twig');
+    }
+    /**
+     * @route("/create", name="provider_create")
+     */
+    public function create(): Response {
+        $provider = new Provider();
+        $form = $this->createForm(ProviderType::class, $provider);
+        $render_parameters = array('provider_form' => $form->createView());
+
+        return $this->render('providers/form/form.html.twig', $render_parameters);
     }
 }
 ?>
